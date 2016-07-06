a69373c53b5af6ebeffe53a7f18fc0193f436948
Danis42 <ciammo@web.de>
2016-07-06 17:21:56 +0200

    cesar fix

--- cypher/cesar_c3.py
+++ cypher/cesar_c3.py
@@ -1,15 +1,16 @@
+print("Please select the third Cipher Text (c3.txt)")
 fileind = input("file path: ")
 
 alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/']
 i=0
 
 while i<len(alpha):
-	print(i)
 	txt = open(fileind)
+	print(i)
+	print()
 	while (True):
 		c = txt.read(1)
-		if not c:
-			print("END OF FILE !!")
+		if (not c) or (c=="!"):
 			break
 		try:
 			if((int(alpha.index(c))+i)<len(alpha)):
@@ -23,6 +24,7 @@ while i<len(alpha):
 	i=i+1
 
 
+print("Content of c3.txt")
 print("")
 txt = open(fileind)
 while (True):
@@ -31,3 +33,6 @@ while (True):
 		print("end")
 		break
 	print(c,end="")
+
+
+#Solution: The Cesar cipher is moved by 16 
